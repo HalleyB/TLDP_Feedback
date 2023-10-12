112c0257d40341b18041610df66d25dce56b36e0
Alice Smith <alice.smith.com>
2023-10-12 19:46:20 -0400

    Full manager functionality

--- client/feedback/src/children/managerAddResponse.js
+++ client/feedback/src/children/managerAddResponse.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
@@ -6,10 +7,23 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
 import Modal from "./modal";
+import { Button } from '@mui/material';
 
 
 
 function ManagerResponse(props) {
+
+    const [newResponse, setResponse] = useState({
+        feedback_id: 0, manager_id: props.userId, date: '', 
+        employee_id: 0, feedback: '', response: ''
+    })
+    const [resError, setResError] = useState(false)
+
+    const setDate = () => {
+        let date = new Date()
+        date = date.toISOString().split('T')[0]
+        setResponse({...newResponse, date: date})
+    }
 
     if (!props.show) {
         return null;
@@ -24,7 +38,47 @@ function ManagerResponse(props) {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center'
-      };
+    };
+
+    const resetForm = () => {
+        setResponse({
+            feedback_id: 0, manager_id: props.userId, date: '', 
+            employee_id: 0, feedback: '', response: ''
+        })
+    }
+
+    const handleChange = (event) => {
+        let selectedFeedback = event.target.value;
+        let employeeId = selectedFeedback.employee_id;
+        let feedback = selectedFeedback.feedback;
+        let feedbackId = selectedFeedback.feedback_id;
+        setResponse({...newResponse, employee_id: employeeId, 
+            feedback: feedback, feedback_id: feedbackId})
+    }
+
+    const handleResponse = (e) => {
+        let curDate = setDate()
+        setResponse({...newResponse, response: e.target.value, date: curDate})
+    }
+
+    const handleSubmit = () => {
+        if (!newResponse.response || !newResponse.employee_id) {
+            setResError(true)
+        } else {
+            fetch(`/api/${props.userId}/managerResponse`, {
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newResponse)
+            })
+            .then(data => {
+                props.setShow(false)
+                props.setNewData(true)
+                resetForm()
+            })
+        }
+    }
 
     return (
     <Box
@@ -45,10 +99,17 @@ function ManagerResponse(props) {
                             id="select-feedback"
                             value={props.feedbackId}
                             label="Feedback Id"
+                            onChange={handleChange}
+                            
 >
-                            <MenuItem value={10}>Ten</MenuItem>
-                            <MenuItem value={20}>Twenty</MenuItem>
-                            <MenuItem value={30}>Thirty</MenuItem>
+                            {props.unansweredFeedback.length ? props.unansweredFeedback.map((oneFeedback) => {
+                                return(
+                                    <MenuItem value={oneFeedback} >
+                                        {oneFeedback.feedback}
+                                    </MenuItem>
+                                )
+                            }) : <MenuItem disabled>No unanswered feedback</MenuItem>}
+                            
                         </Select>
                         <TextField
                             id="outlined-multiline-static"
@@ -57,8 +118,12 @@ function ManagerResponse(props) {
                             fullWidth
                             rows={4}
                             defaultValue=""
+                            onChange={(e) => handleResponse(e)}
 />
+                        <Button onClick={() => handleSubmit()}>Submit</Button>
+                        <Button onClick={() => props.setShow(false)}>Cancel</Button>
                     </FormControl>
+                    {resError ? 'Please ensure all boxes are filled out' : ''}
                 </div>
             </Modal>
         </div>
