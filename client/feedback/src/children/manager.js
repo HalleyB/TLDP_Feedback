710eb86a9c650d3e0af374e11202a38a38482778
Alice Smith <alice.smith.com>
2023-10-12 21:19:42 -0400

    More manager functionality

--- client/feedback/src/children/manager.js
+++ client/feedback/src/children/manager.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect} from 'react';
+import { Button } from '@mui/material';
 import ManagerResponse from './managerAddResponse';
+import Modal from './modal';
 
 
 
@@ -9,6 +11,20 @@ function Manager(props) {
     const [pastResponses, setPastResponses] = useState([])
     const [unansweredFeedback, setUnansweredFeedback] = useState([])
     const [newData, setNewData] = useState(false)
+    const [showFeedback, setShowFeedback] = useState(false)
+    const [currentFeedback, setCurrentFeedback] = useState({})
+
+    const modalStyles = {
+        position: 'fixed',
+        left: '0',
+        right: '0',
+        top: '0',
+        bottom: '0',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    };
 
     const getPastFeedback = () => {
         fetch(`/api/${props.userId}/managerGetFeedback`)
@@ -63,6 +79,8 @@ function Manager(props) {
             setNewData(false)
         }
     }, [newData])
+    
+    useEffect(() => {}, [showFeedback])
 
     if (!props.show) {
         return null;
@@ -71,9 +89,9 @@ function Manager(props) {
     return (<div className='parent'>
         <h1>Welcome {props.userInfo.employee_info.name}</h1>
         <div className='past-feedback'>
-            <h3>Feedback Received</h3>
+            <h3>Unanswered Feedback</h3>
             <div className='feedback'>
-            {pastFeedback.map((feedbackObject, index) => {
+            {unansweredFeedback.map((feedbackObject, index) => {
                 return (
                     <p key={index}>
                     {feedbackObject.sentiment} Feedback #{feedbackObject.feedback_id}: {feedbackObject.feedback}
@@ -87,14 +105,27 @@ function Manager(props) {
             <div className='responses'>
             {pastResponses.map((responseObject, index) => {
                 return (
-                    <p key={index}>
+                    <p key={index} onClick={() => {
+                        console.log('here')
+                        setShowFeedback(true)
+                        setCurrentFeedback(responseObject)
+                    }}>
                     Responded to Feedback #{responseObject.feedback_id}: {responseObject.response}
                     </p>
                 )
             })}
             </div>
         </div>
-        
+
+        <Modal styles={modalStyles} show={showFeedback}>
+            <div className='modal-body'>
+                <h3>Feedback</h3>
+                {currentFeedback.feedback}
+                <h3>Response</h3>
+                {currentFeedback.response}
+                <Button onClick={() => setShowFeedback(false)}>Close</Button>
+            </div>
+        </Modal>
         <button className="button" onClick={() => setShowModal(true)}>Add New Response</button>
         <ManagerResponse show={showModal} setShow={setShowModal} setNewData={setNewData}
         unansweredFeedback={unansweredFeedback} userId={props.userId}/>
